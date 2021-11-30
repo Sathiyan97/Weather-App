c773abde1475ff945497d41722e01d0ee07df2a0
sathiyan.A <saththiyan97@gmail.com>
2021-12-01 01:15:24 +0530

    Login created

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+import {Login} from "./pages/Login";
+import {Home} from "./pages/Home";
+
+function App() {
+  return (
+      <BrowserRouter>
+          <Routes>
+
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+
+          </Routes>
+      </BrowserRouter>
+
+  );
+}
+
+export default App;
